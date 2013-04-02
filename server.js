772a1532982d2c9928e9807ddc0b0b8f4ba447aa
Mark Phillips <embeepea@git>
2013-04-02 00:00:37 -0400

    adds -w option to server

--- server.js
+++ server.js
@@ -1,20 +1,55 @@
+process.argv.shift();
+process.argv.shift();
+arg = process.argv.shift();
+
+var cgi_mode = false;
+if (arg === "-w") {
+    cgi_mode = true;
+    arg = process.argv.shift();
+}
+var port = arg;
+
+if (!port) {
+    console.log('usage: server PORT');
+    process.exit(1);
+}
+
 var http = require('http');
+var exec = require('child_process').exec;
 
-var server = http.createServer(function(req,res) {
-    res.writeHeader({
-        'Content-Type' : 'text/plain'
-    });
+var php = cgi_mode ? "php-cgi" : "php";
 
+http.createServer(function(req,res) {
+    var prog = req.url.replace(/\?.*$/, '').replace(/^[^\/]*\//, ''),
+        args = req.url.replace(/^.*\?/, '').split("&").join(" "),
+        cmd = php + ' ./' + prog + ' ' + args;
+    exec(cmd,
+         function (error, stdout, stderr) {
+             lines = stdout.split(/\n/);
+             body_lines = [];
+             in_body = false;
+             header = {};
+             for (var i=0; i<lines.length; ++i) {
+                 line = lines[i].replace(/^\s+/, '').replace(/\s+$/, '');
+                 if (in_body) {
+                     body_lines.push(line);
+                 } else {
+                     if (line.length === 0) {
+                         in_body = true;
+                     } else {
+                         m = line.match(/^([^:]+):\s*(.*)*/);
+                         if (m) {
+                             key = m[1];
+                             val = m[2];
+                             header[key] = val;
+                         }
+                     }
+                 }
+             }
 
-    var exec = require('child_process').exec,
-        child;
+             res.writeHeader(200, header);
+             res.end(body_lines.join("\n"));
+         });
+}).listen(port);
 
-    child = exec('ls',
-                 function (error, stdout, stderr) {
-                     res.end(stdout);
-                 });
-
-}).listen(3000);
-
-
-
+console.log('listening on port ' + port + '...');
