019f8c0a6d161a2e8406bec8376f49d311e0cd50
Mark Phillips <embeepea@git>
2013-04-03 12:24:38 -0400

    adds default timezone settings to args.php; updates README

--- args.php
+++ args.php
@@ -1,1 +1,4 @@
-<?php if (isset($argv)) { parse_str(implode('&', array_slice($argv, 1)), $_REQUEST); } ?>
+<?php
+if (isset($argv)) { parse_str(implode('&', array_slice($argv, 1)), $_REQUEST); }
+date_default_timezone_set("America/New_York"); 
+?>
